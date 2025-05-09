ccd10cd5adeaaaa2f1b17ff3917778815a6080bf
Ayyamshah <ayyamshah930@gmail.com>
2025-05-09 18:15:52 +0530

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -18,7 +18,7 @@ const Cart = ({ cart = [], deleteCart, upDateQuantity }) => {
                 <img
                   className="img-fluid object-fit-cover"
                   style={{ width: '100px', height: '150px' }}
-                  src={`/Kisds&Teens/${item.img}`}
+                  src={`/public/${item.img}`}
                   alt=""
                 />
                 <div className="ms-3">
