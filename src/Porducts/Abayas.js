7c05fd0dd6001fc83c3e730a68c1d03668c68b01
Ayyamshah <ayyamshah930@gmail.com>
2025-05-15 12:46:06 +0530

    Update Abayas.js

--- src/Porducts/Abayas.js
+++ src/Porducts/Abayas.js
@@ -2,7 +2,7 @@ export const Abaya = [
     
     {id:1 ,  produst: 'Anitkabir Abaya Black',                   RS:"2500",  DelRS :'3000' ,img:'/Abayas/Abayas01.webp' ,off:'40' },
     {id:2 ,  produst: 'Jardin Abaya White With Jacket',          RS:"5000",  DelRS :'6500' ,img:'/Abayas/Abayas02.webp' },
-    {id:3 ,  produst: 'Abadi Kaftan Black',                      RS:"3800",  DelRS :'4000' ,img:'/Abayas/Abayas03.webp  },
+    {id:3 ,  produst: 'Abadi Kaftan Black',                      RS:"3800",  DelRS :'4000' ,img:'/Abayas/Abayas03.webp' },
     {id:4 ,  produst: 'Buchra Abaya Black',                      RS:"3700",  DelRS :'4500' ,img:'/Abayas/Abayas04.webp' },
     {id:5 ,  produst: 'Amrabat Kaftan Neel Blue',                RS:"3000",  DelRS :'3500' ,img:'/Abayas/Abayas05.webp' },
     {id:6 ,  produst: 'Nayef Coat Style Abaya Navy Blue',        RS:"3800",  DelRS :'3900' ,img:'/Abayas/Abayas06.webp' },
