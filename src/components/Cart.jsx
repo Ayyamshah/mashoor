779ae80a2730eead0ef051a0eba24d30dfd29574
Ayyamshah <ayyamshah930@gmail.com>
2025-05-15 13:08:40 +0530

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -18,8 +18,8 @@ const Cart = ({ cart = [], deleteCart, upDateQuantity }) => {
                 <img
                   className="img-fluid object-fit-cover"
                   style={{ width: '100px', height: '150px' }}
-                  src={`/${item.img}`} // ✅ Proper image path
-                  alt=""
+                  <img src={`${process.env.PUBLIC_URL}/${item.img}`} alt={item.produst} />
+
                 />
                 <div className="ms-3">
                   <h4>{item.produst}</h4>
